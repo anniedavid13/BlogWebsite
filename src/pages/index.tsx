e2f12b668edc9e11a46f6e40bc97e9187ace597f
Annie David <E20CSE068@bennett.edu.in>
2023-03-29 15:55:37 +0530

    setting up files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg-col-span-8 col-span-1">
         {posts.map((post,index)=>(
-          <div>
+          <div> 
             {post.title}
             {post.excerpt}
           </div>
         ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className = "lg:sticky lg:top-8">
+         </div>
+        </div>
       </div>
+
       </div>
   )
 }

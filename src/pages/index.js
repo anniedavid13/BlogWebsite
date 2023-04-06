47575a2f736d782b11aa8f3551d77fd10fe38edc
Annie David <E20CSE068@bennett.edu.in>
2023-04-06 22:28:58 +0530

    made sidemenu items

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,31 +10,28 @@ import {getPosts} from "../services"
 //   {title: "Post 3", excerpt: "post-3"},
 // ]
 
-export default function Home({posts}) {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 ">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="container mx-auto px-10 mb-8">
+      {/* <FeaturedPosts /> */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg-col-span-8 col-span-1">
-        {posts.map((post)=>(
-          <PostCard key={post.title} post={post.node}/>
-        ))}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className = "lg:sticky lg:top-8">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
-         </div>
+          </div>
         </div>
       </div>
-
-      </div>
-  )
+    </div>
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
